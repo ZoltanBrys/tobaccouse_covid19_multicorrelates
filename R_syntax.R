913828101da1d5e8ae678a75a59b175d1e48bbdd
Zoltan Brys <103111650+ZoltanBrys@users.noreply.github.com>
2025-11-03 14:58:52 +0100

    R-code

--- R_syntax.R
+++ R_syntax.R
@@ -1,298 +1,186 @@
-# METADATA 
-########################################################################################################
-# Related manuscript  : Comparing the correlates of daily tobacco use 
-#               and COVID-19 vaccination refusal among the Hungarian active population
-# R-Code      : Zoltan Brys
-# Closed      : -  
-########################################################################################################
+# ---- ABOUT ----
+# Title of the manuscript : 
+#    Comparing correlates of daily tobacco use and COVID-19 vaccination refusal 
+#    in the working-age Hungarian population
+# R-Code      : anonymized
+# Closed      : 3rd of November, 2025  
 
 
-# PACKAGES AND ENVIRONMENT
-########################################################################################################
+# ---- PACKAGES AND ENVIRONMENT ----
 #preparation
   rm(list = ls())
-  if (!("stats" %in% (.packages()) )) stop("R Environment is not fully loaded!")
+  gc(reset = TRUE)
 
 #packages
-  library(Matrix)
-  library(MASS) #for CIs
-  library(fmsb) #for Nagelkerke
-  library(glmnet) #for lasso.
-  library(car) #for bootstrap.
-  library(rcompanion) #for effect sizes
-  library(margins) #for AME
-  library(pROC) #for ROC-AUC 95% CI
-
-#function for calculating Jaccard similarity measure
-  jaccardc <- function (x, y) 
-  {
-    
-    if (anyNA(x)) stop("NAs in x")
-    if (anyNA(y)) stop("NAs in y")
-    if (length(x) != length(x)) stop("Lenghts are non-equal!")
-    
-    c11 = sum(x == 1 & y == 1)
-    c10 = sum(x == 1 & y == 0)
-    c01 = sum(x == 0 & y == 1)
-    
-    return (c11 / (c11 + c10 + c01))
-  }
-########################################################################################################
-
-
-# LOADING AND CHECKING PREPARED INPUT DATA 
-########################################################################################################
-  C19 <- readRDS("C19.RDS")
-#it contains the variables described in Table1 y1,y2 and x1, x2...x21.
-# variable sorsz is the common key variable with the open KDK dataset
-
-#check the input data
-  if (!exists("C19"))             stop("Input data was not loaded!")
-  if (class(C19) != "data.frame") stop("Input data format is not correct!")
-########################################################################################################
-
-
-# Characteristics of the sample (raw data for Table 2.)
-########################################################################################################
-  C19_sum_raw <- summary(C19)
-  print(C19_sum_raw) 
-########################################################################################################
-
-
-# Everyday tobacco use and COVID-19 vaccination refusal (A1)
-########################################################################################################
-#bivariate
-  y1y2sm <- glm(y2 ~ y1, family=binomial(link='logit') , data=C19) 
-  summary(y1y2sm) 
-  exp(coefficients(y1y2sm)[2])
-  exp(confint(y1y2sm))
-  NagelkerkeR2(y1y2sm)
-
-#multivariate
-  y1y2cm <- glm(y1 ~ y2 + x3 + x4, family=binomial(link='logit') , data=C19) 
-  summary(y1y2cm)
-  exp(coefficients(y1y2cm)[2])
-  exp(confint(y1y2cm))
-  NagelkerkeR2(y1y2cm)
-#since 95% CI boostrap is stochastic, small difference are to be expected
-
-#Jaccard
-  jxy <- na.omit(subset(C19, select= c("y1", "y2")))
-  jaccardc(jxy$y1, jxy$y2)
-########################################################################################################
-
-
-# Correlates of everyday tobacco use (A2)
-########################################################################################################
-#scaling the data
-  A2C19 <- scale(na.omit(C19[ ,c(-1,-3)]))
-
-#A2 -- first LASSO model for visualization
-  glmmodA2 <- glmnet(as.matrix(A2C19[,-1]), 
-                     as.matrix(A2C19[,1]), 
-                     alpha = 1, 
-                     family = "binomial",
-                     intercept = FALSE)
+  library(data.table)
+  library(dplyr)
   
-#result objects for A2
-  A2bet <- NULL #results betas
-  A21se <- NULL #results lambda1se
-  A2ROC <- NULL #results ROC
-
-#1000 times repeated beta/l1se/ROC-AUC calculations of A2 with intercept set to 0
-  for (c1 in 1:1000)
-    {
-    #finding lambda.1se
-    cv_model1 <- cv.glmnet(as.matrix(A2C19[,-1]), 
-                           as.matrix(A2C19[,1]), 
-                           alpha = 1, 
-                           family="binomial", 
-                           kfold=5 ,    
-                           type.measure = "auc",
-                           intercept = FALSE) 
-
-    #evaulating the model at lambda.1se
-    glmmod2 <- glmnet(as.matrix(A2C19[,-1]), 
-                      as.matrix(A2C19[,1]), 
-                      alpha = 1, 
-                      family = "binomial", 
-                      lambda = cv_model1$lambda.1se,
-                      intercept = FALSE)
-    
-    #saving results of each model
-    A2bet <- rbind(A2bet, t(as.matrix(coef(glmmod2))))
-    A21se <- rbind(A21se, cv_model1$lambda.1se)
-    A2ROC <- rbind(A2ROC, cv_model1$cvm[which(cv_model1$lambda.1se==cv_model1$lambda)])
-  }
-
+  library(car)
+  library(VGAM)
+  library(PRROC) 
   
-  #Plot figure 2A
-  png(filename = "Figure2A.png",
-      width = 1280, height = 960, units = "px", 
-      pointsize = 3, res=600,
-      bg = "white"
-  )
-  
-  plot(glmmodA2, xvar="lambda", label=TRUE, , ylab="", xlab="ln(λ)", 
-       cex.lab=1, cex.axis=1, cex.main=1, cex.sub="1", ylim = c(-1, 1))
-  abline(v = log(quantile(A21se, probs = 0.50)), 
-         col=c("blue", "blue"), 
-         lty=2, 
-         lwd=1)
-  
-  dev.off()
-########################################################################################################
 
+# ---- LOADING INPUT DATA ----
+  C19 <- as.data.table(readRDS("C19.RDS")) 
+
+  yvarnames = paste0("y", 1:2)
+  xvarnames = paste0("x", 1:21)
  
-# Correlates of  of COVID-19 vaccination refusal (A3)
-########################################################################################################
-#scaling the data
-  A3C19 <- scale(na.omit(C19[ ,c(-1,-2)]))
-
-#A3
-  glmmodA3 <- glmnet(as.matrix(A3C19[,-1]), 
-                     as.matrix(A3C19[,1]), 
-                     alpha = 1, 
-                     family = "binomial",
-                     intercept = FALSE)
-
-#results objects for A2
-  A3bet <- NULL #betas
-  A31se <- NULL #lambda 1 se
-  A3ROC <- NULL #ROC values
-
-#1000 times repeated Beta calcualtions
-  for (c1 in 1:1000)
-    {
-      #finding lambda.1se
-      cv_model1 <- cv.glmnet(as.matrix(A3C19[,-1]), 
-                             as.matrix(A3C19[,1]), 
-                             alpha = 1, 
-                             family="binomial", 
-                             kfold=5 ,    
-                             type.measure = "auc",
-                             intercept = FALSE) 
-      
-      #evaulating the model at lambda.1se
-      glmmod2 <- glmnet(as.matrix(A3C19[,-1]), 
-                        as.matrix(A3C19[,1]), 
-                        alpha = 1, 
-                        family = "binomial", 
-                        lambda = cv_model1$lambda.1se,
-                        intercept = FALSE)
-      
-      #saving tesults of c1 model
-      A3bet <- rbind(A3bet, t(as.matrix(coef(glmmod2))))
-      A31se <- rbind(A31se, cv_model1$lambda.1se)
-      A3ROC <- rbind(A3ROC, cv_model1$cvm[which(cv_model1$lambda.1se==cv_model1$lambda)])
-  }
+  
+# ---- Descriptive statistics ----
+  table2 <- summary(C19)
+  print("Table 2.")
+  print(table2) 
 
 
-#Plot figure 2B
-  png(filename = "Figure2B.png",
-       width = 1280, height = 960, units = "px", 
-       pointsize = 3, res=600,
-       bg = "white"
+# ---- Data Imputation ----
+#y1,y2 are not imputed, edu coding of x3,x4 considered
+  C19_imp <- C19 %>%
+    filter(!is.na(y1) & !is.na(y2)) %>%
+    mutate(edu_group = case_when(
+      x3 == 0 & x4 == 0 ~ "alapfoku",
+      x3 == 1 & x4 == 0 ~ "kozepfoku",
+      x3 == 1 & x4 == 1 ~ "felsofoku",
+    )) %>%
+    group_by(edu_group) %>%
+    mutate(across(all_of(xvarnames), ~ ifelse(is.na(.), median(., na.rm = TRUE), .))) %>%
+    ungroup() %>%
+    select(-edu_group) %>%
+    select(-sorsz) #remove uid
+#remove C19, not to analyze accidentaly
+  rm(C19)
+
+
+# ---- Scaling, for comparable AMEs ----
+  C19_imp <- C19_imp %>%
+    mutate(across(.cols = setdiff(names(.)[sapply(., is.numeric)], c("y1", "y2")),
+                  .fns = ~ as.numeric(scale(.))))  
+
+
+# ---- Multivariate modeling ----  
+#y1~y2
+  print("Phi(y1~y2)=")
+  cor(C19_imp$y1, C19_imp$y2)
+  
+  print("JI(y1~y2)=")
+  jaccard_index <- function(x, y) { sum(x & y) / sum(x | y) }
+  jaccard_index(C19_imp$y1, C19_imp$y2)
+
+#VIF  
+  lm_vif_y1 <- glm(y1 ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + 
+      x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21, 
+                data = C19_imp, 
+                family = binomial)
+  
+  lm_vif_y2 <- glm(y2 ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + 
+      x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21, 
+                   data = C19_imp, 
+                   family = binomial)
+  
+  vif_y1 <- car::vif(lm_vif_y1)
+  vif_y2 <- car::vif(lm_vif_y2)
+  
+  print("VIFs")
+  summary(cbind(vif_y1, vif_y2))
+  
+# def y
+  y_mat <- as.matrix(C19_imp[, c("y1", "y2")])
+
+# def null_mod
+  null_mod <-  vglm(
+    y_mat ~ 1,
+    family = binomialff(multiple.responses = TRUE),
+    data = C19_imp
   )
+  print("Null modell:")
+  summary(null_mod)
   
-  plot(glmmodA3, xvar="lambda", label=TRUE, , ylab="", xlab="ln(λ)", 
-       cex.lab=1, cex.axis=1, cex.main=1, cex.sub="1", ylim = c(-1, 1))
-  abline(v = log(quantile(A31se, probs = 0.50)), 
-         col=c("blue", "blue"), 
-         lty=2, 
-         lwd=1)
+
+#full mod
+  fit_mv_logit_vgam <- VGAM::vglm(
+    y_mat ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + 
+      x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21,
+    family = binomialff(multiple.responses = TRUE),
+    data = C19_imp
+  )
+  print("Full modell:")
+  summary(fit_mv_logit_vgam)
+
+#aORs
+  se <- sqrt(diag(vcov(fit_mv_logit_vgam)))
   
-  dev.off()
-########################################################################################################  
-
-      
-# Comparison of social factors behind tobacco use and COVID-19 vaccination refusal (A4)
-########################################################################################################
-#ROC-AUC - table 3 model parameters
-  summary(A2ROC)[c(1,3,6),]
-  summary(A3ROC)[c(1,3,6),]
-  wilcox.test(A2ROC, A3ROC)
-  wilcoxonZ(A2ROC, A3ROC)
-  abs(wilcoxonZ(A2ROC, A3ROC))/ sqrt(1000)
+  log_odds_vec <- coef(fit_mv_logit_vgam)
+  lower <- log_odds_vec - 1.96 * se
+  upper <- log_odds_vec + 1.96 * se
   
-#distribution of lambda 1se - table 3 model parameters
-  summary(A21se)[c(1,3,6),]
-  summary(A31se)[c(1,3,6),]
-
-#table 3 betas
-  table3 <- data.frame(
-    varname = character(22), 
-    a2med   = numeric(22), 
-    a2min   = numeric(22),
-    a2max   = numeric(22),
-    a3med   = numeric(22), 
-    a3min   = numeric(22), 
-    a3max   = numeric(22)
-    )
-
-  for (c3 in 1:22)
-    {
-    table3$varname[c3] <- paste0("x",c3-1)
-    
-    table3$a2min[c3]  <- min(A2bet[,c3])
-    table3$a2med[c3]  <- median(A2bet[,c3])
-    table3$a2max[c3]  <- max(A2bet[,c3])
-    
-    table3$a3min[c3]  <- min(A3bet[,c3])
-    table3$a3med[c3]  <- median(A3bet[,c3])
-    table3$a3max[c3]  <- max(A3bet[,c3])
-  }
-
-
-#table3
-  table3r <- round(table3[,2:7], digits=1)
-  rownames(table3r) <- table3$varname
-  table3
-
-#comparing perceived social norms betas
-  wilcox.test(A2bet[,21], A3bet[,22])
-  wilcoxonZ(A2bet[,21], A3bet[,22])
-  abs(wilcoxonZ(A2bet[,21], A3bet[,22]))/ sqrt(1000)
+  aor_ci <- data.frame(
+    aOR = round(exp(log_odds_vec), 2),
+    aOR_lower = round(exp(lower), 2),
+    aOR_upper = round(exp(upper), 2)
+  )
+  aor_ci$str=paste0(aor_ci$aOR,"(",aor_ci$aOR_lower,"–", aor_ci$aOR_upper,")")
   
-########################################################################################################
+  print("Adjusted Odds Ratios with 95% CI:")
+  print(aor_ci)
+  
+#AMEs
+  pred <- predict(fit_mv_logit_vgam, type = "response")  
+  coefs <- coef(fit_mv_logit_vgam, matrix = TRUE) 
+  avg_grad <- colMeans(pred * (1 - pred))
+  ame_matrix <- sweep(coefs, 2, avg_grad, FUN = "*")
+  ame_xvars <- ame_matrix[rownames(coefs) %in% xvarnames, , drop = FALSE]
+  print("AMEs:")
+  print(round(ame_xvars,3))
 
+#LL
+  ll_null <- logLik(null_mod)
+  ll_full <- logLik(fit_mv_logit_vgam)
+  
+  df_null <- length(coef(null_mod))
+  df_full <- length(coef(fit_mv_logit_vgam))
+  
+  LR_stat <- 2 * (as.numeric(ll_full) - as.numeric(ll_null))  
+  df_diff <- df_full - df_null
+  
+  pval <- pchisq(LR_stat, df = df_diff, lower.tail = FALSE)
+  
+  print("Likelihood-ratio test:")
+  print(paste("LogLik null model:", round(as.numeric(ll_null), 2)))
+  print(paste("LogLik full model:", round(as.numeric(ll_full), 2)))
+  print(paste("LR Chi-square:", round(LR_stat, 2)))
+  print(paste("df:", df_diff))
+  print(paste("p-value:", signif(pval, 3)))
+  
+#AIC
+  print("AIC-null-modell:")
+  print(round(AIC(null_mod),2))
+  print("AIC-full-modell:")
+  print(round(AIC(fit_mv_logit_vgam),2))
+  
+#ROC-AUC and PR-AUC
+  pred_y1 <- pred[, 1]
+  pred_y2 <- pred[, 2]
 
-# Sensitivity analysis (A4+)
-########################################################################################################
-# fit  logistic regression model y1 ~ x4 + x20 + x21 ; y2 ~ x4 + x20 + x21
-  A2AMEm <- glm(y1 ~ x4 + x20 + x21, data = C19, family = binomial)
-  A3AMEm <- glm(y2 ~ x4 + x20 + x21, data = C19, family = binomial)
+prroc_metrics <- function(scores, labels) {
+  s0 <- scores[labels == 0]  # negatives
+  s1 <- scores[labels == 1]  # positives
 
-# Average Marginal Effects of y1 ~ x4 + x20 + x21 ; y2 ~ x4 + x20 + x21
-  A2AME <- margins(A2AMEm, vce="bootstrap")
-  A3AME <- margins(A3AMEm, vce="bootstrap")
+  # IMPORTANT: for ROC, pass POSITIVES FIRST
+  roc <- PRROC::roc.curve(scores.class0 = s1, scores.class1 = s0, curve = TRUE)
+  pr  <- PRROC::pr.curve(scores.class0 = s0, scores.class1 = s1, curve = TRUE)
 
-#AME values y1 ~ x4 + x20 + x21 ; y2 ~ x4 + x20 + x21
-  summary(A2AME)
-  summary(A3AME)
+  list(roc_auc = roc$auc, pr_auc = pr$auc.integral,
+       pr_curve = pr, roc_curve = roc)
+}
 
-#Nagelkerke y1 ~ x20
-  y1x20 <- glm(y1 ~ x20 , family=binomial(link='logit') , data=C19) 
-  summary(y1x20)  
-  exp(coefficients(y1x20)[2])
-  exp(confint(y1x20))
-  NagelkerkeR2(y1x20)
+m1 <- prroc_metrics(pred_y1, C19_imp$y1)
+cat("ROC-AUC for y1:", round(m1$roc_auc, 2), "\n")
+cat("PR-AUC  for y1:", round(m1$pr_auc,  2), "\n")
+cat("PR-AUC baseline (prevalence) for y1:", round(mean(C19_imp$y1), 2), "\n")
 
-#ROC-AUC y1 ~ x20
-  roc_result <- roc(response = y1x20$model$y1, predictor = predict(y1x20, type = "response"))
-  auc_result <- auc(roc_result)
-  ci.auc(roc_result, method = "bootstrap", of = "AUC", percent = 0.95)
+m2 <- prroc_metrics(pred_y2, C19_imp$y2)
+cat("ROC-AUC for y2:", round(m2$roc_auc, 2), "\n")
+cat("PR-AUC  for y2:", round(m2$pr_auc,  2), "\n")
+cat("PR-AUC baseline (prevalence) for y2:", round(mean(C19_imp$y2), 2), "\n")
 
-#Nagelkerke y2 ~ x21
-  y2x21 <- glm(y2 ~ x21, family=binomial(link='logit') , data=C19) 
-  summary(y2x21 ) 
-  exp(coefficients(y2x21)[2])
-  exp(confint(y2x21))
-  NagelkerkeR2(y2x21)
-
-#ROC-AUC y2 ~ x21
-  roc_result <- roc(response = y2x21$model$y2, predictor = predict(y2x21, type = "response"))
-  auc_result <- auc(roc_result)
-  ci.auc(roc_result, method = "bootstrap", of = "AUC", percent = 0.95)
-#since 95% CI boostrap is stochastic, small difference are to be expected
-########################################################################################################
+stop("Code run completed.")  
